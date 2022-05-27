f3388e7a7b5e901a729bc3b5ad4f2d7c71ec0f08
Mayank Pant <mpant4512@gmail.com>
2022-05-28 02:34:21 +0530

    Added and placed score labels

--- gui-rps.py
+++ gui-rps.py
@@ -2,10 +2,17 @@ from tkinter import *
 import random
 
 window = Tk()
+
 player1 = StringVar()
 player1.set("Player1")
 player2 = StringVar()
 player2.set("Player2")
+
+score1 = IntVar()
+score1.set(0)
+score2 = IntVar()
+score2.set(0)
+
 title_frame = Frame(window, padx=5, pady=5)
 body_frame = Frame(window, padx=5, pady=5)
 player1_frame = Frame(window, padx=5, pady=5)
@@ -15,13 +22,18 @@ title_label = Label(title_frame, text="ROCK PAPER SCISSORS", foreground="Orange"
 player_label1 = Label(body_frame, textvariable=player1, foreground="Orange", height=3, font=("Bold", 20, ""))
 player_label2 = Label(body_frame, textvariable=player2, foreground="Orange", height=3, font=("Bold", 20, ""))
 
+score_label1 = Label(body_frame, textvariable=score1, foreground="Orange", height=3, font=("Bold", 20, ""))
+score_label2 = Label(body_frame, textvariable=score2, foreground="Orange", height=3, font=("Bold", 20, ""))
+
 title_label.grid(row=0, column=0)
 title_frame.grid(row=0, column=0)
 body_frame.grid(row=1, column=0)
 player_label1.grid(row=0, columnspan=3, column=0)
-player_label2.grid(row=0, columnspan=3, column=8)
+player_label2.grid(row=0, columnspan=3, column=7)
 player1_frame.grid(row=1, column=0, columnspan=6)
 player2_frame.grid(row=1, column=6, columnspan=6)
+score_label1.grid(row=2, column=2)
+score_label2.grid(row=2, column=8)
 
 player_label1.grid()
 window.mainloop()
