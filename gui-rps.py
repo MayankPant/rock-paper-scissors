2c3794dd00b879bd5ea6602a34a2871821ab13c9
Mayank Pant <mpant4512@gmail.com>
2022-05-28 02:27:50 +0530

    Added lines to place  the image frames and player names
    Divided the bodyframe into 12 seperate columns and
    then tried positioning the player name labels and
    image frames respect to these using rowspans.

--- gui-rps.py
+++ gui-rps.py
@@ -1,26 +1,27 @@
 from tkinter import *
 import random
 
-player1 = StringVar()
-player1.set("")
-player2 = StringVar()
-player2.set("")
-
-
-
 window = Tk()
-title_frame = Frame(window, padx=5, pady = 5)
-body_frame = Frame(window, padx = 5, pady=5)
-player1_frame = Frame(window, padx = 5, pady=5)
-player2_frame = Frame(window, padx = 5, pady=5)
-title_label = Label(title_frame, text="ROCK PAPER SCISSORS", foreground="Orange", height=3, font = ("Bold",20,""))
+player1 = StringVar()
+player1.set("Player1")
+player2 = StringVar()
+player2.set("Player2")
+title_frame = Frame(window, padx=5, pady=5)
+body_frame = Frame(window, padx=5, pady=5)
+player1_frame = Frame(window, padx=5, pady=5)
+player2_frame = Frame(window, padx=5, pady=5)
+title_label = Label(title_frame, text="ROCK PAPER SCISSORS", foreground="Orange", height=3, font=("Bold", 20, ""))
 
-player_label1 = Label(body_frame, textvariable=player1, foreground="Orange", height=3, font = ("Bold",10,""))
-player_label2 = Label(body_frame, textvariable=player2, foreground="Orange", height=3, font = ("Bold",10,""))
+player_label1 = Label(body_frame, textvariable=player1, foreground="Orange", height=3, font=("Bold", 20, ""))
+player_label2 = Label(body_frame, textvariable=player2, foreground="Orange", height=3, font=("Bold", 20, ""))
 
 title_label.grid(row=0, column=0)
 title_frame.grid(row=0, column=0)
 body_frame.grid(row=1, column=0)
+player_label1.grid(row=0, columnspan=3, column=0)
+player_label2.grid(row=0, columnspan=3, column=8)
+player1_frame.grid(row=1, column=0, columnspan=6)
+player2_frame.grid(row=1, column=6, columnspan=6)
 
 player_label1.grid()
 window.mainloop()
