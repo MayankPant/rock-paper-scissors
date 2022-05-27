9fae802780e45136d62fb259ec0f8c8905338ef3
Mayank Pant <mpant4512@gmail.com>
2022-05-28 01:48:43 +0530

    Created a Title for the game.

--- gui-rps.py
+++ gui-rps.py
@@ -3,6 +3,12 @@ import random
 
 
 window = Tk()
+title_frame = Frame(window, padx=5, pady = 5)
+body_frame = Frame(window, padx = 5, pady=5)
+title_label = Label(title_frame, text="ROCK PAPER SCISSORS", foreground="Orange", height=3, font = ("Bold",20,""))
 
 
+title_label.grid(row=0, column=0)
+title_frame.grid(row=0, column=0)
+body_frame.grid(row=1, column=0)
 window.mainloop()
