511edf2f930d6d53d0c3c919f2db3c2a624ecb1c
Mayank Pant <mpant4512@gmail.com>
2022-05-28 01:54:32 +0530

    Added two frames for two players
    Here we added two frames player1_frame and player2_frame.
    The aim is to host images of a move corresponding to what
    a user chooses. For instance, if player 1 chooses rock, we
    show a rock in its corresponding frame and at the same time
    if player2 chooses paper, we show an image of a paper at its
    corresponding frame.

--- gui-rps.py
+++ gui-rps.py
@@ -5,10 +5,13 @@ import random
 window = Tk()
 title_frame = Frame(window, padx=5, pady = 5)
 body_frame = Frame(window, padx = 5, pady=5)
+player1_frame = Frame(window, padx = 5, pady=5)
+player2_frame = Frame(window, padx = 5, pady=5)
 title_label = Label(title_frame, text="ROCK PAPER SCISSORS", foreground="Orange", height=3, font = ("Bold",20,""))
 
 
 title_label.grid(row=0, column=0)
 title_frame.grid(row=0, column=0)
 body_frame.grid(row=1, column=0)
+
 window.mainloop()
