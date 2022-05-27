24789bce500d637cbc3777c435376cc7503c7025
Mayank Pant <mpant4512@gmail.com>
2022-05-28 02:11:56 +0530

    Added two name variables for players

--- gui-rps.py
+++ gui-rps.py
@@ -1,6 +1,12 @@
 from tkinter import *
 import random
 
+player1 = StringVar()
+player1.set("")
+player2 = StringVar()
+player2.set("")
+
+
 
 window = Tk()
 title_frame = Frame(window, padx=5, pady = 5)
@@ -9,9 +15,12 @@ player1_frame = Frame(window, padx = 5, pady=5)
 player2_frame = Frame(window, padx = 5, pady=5)
 title_label = Label(title_frame, text="ROCK PAPER SCISSORS", foreground="Orange", height=3, font = ("Bold",20,""))
 
+player_label1 = Label(body_frame, textvariable=player1, foreground="Orange", height=3, font = ("Bold",10,""))
+player_label2 = Label(body_frame, textvariable=player2, foreground="Orange", height=3, font = ("Bold",10,""))
 
 title_label.grid(row=0, column=0)
 title_frame.grid(row=0, column=0)
 body_frame.grid(row=1, column=0)
 
+player_label1.grid()
 window.mainloop()
